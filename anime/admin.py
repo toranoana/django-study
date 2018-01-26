ee0861d97f0c5626e39a363bae563e5a7453825f
Takaaki Shirai <lain.op.rai@gmail.com>
2018-01-26 22:33:14 +0900

    声優モデルの追加

--- anime/admin.py
+++ anime/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Anime
+from .models import Anime, CV, AnimeCV
 
 admin.site.register(Anime)
+admin.site.register(CV)
+admin.site.register(AnimeCV)
--- anime/models.py
+++ anime/models.py
@@ -2,6 +2,19 @@ from django.db import models
 
 
 class Anime(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
     year = models.IntegerField()
     season = models.IntegerField() # 1, 4, 7, 10
+
+
+class CV(models.Model):
+    name = models.CharField(max_length=50)
+    birthday = models.DateField()
+    sex = models.BooleanField()
+    agency = models.CharField(max_length=50)
+    official_url = models.CharField(max_length=255)
+
+
+class AnimeCV(models.Model):
+    anime = models.ForeignKey(Anime, on_delete=models.DO_NOTHING)
+    cv = models.ForeignKey(CV, on_delete=models.DO_NOTHING)
