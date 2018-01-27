04ad828b531899edc8906f351da104f0b8c0b0dc
Takaaki Shirai <lain.op.rai@gmail.com>
2018-01-27 15:55:41 +0900

    アニメ詳細ページを追加して出演情報を表示

--- anime/views.py
+++ anime/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Anime
+from .models import Anime, CV, AnimeCV
 
 
 def index(request):
@@ -9,5 +9,18 @@ def index(request):
     template = loader.get_template('anime_list.html')
     context = {
         'anime_list': anime_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def anime(request, anime_id):
+    anime = Anime.objects.get(pk=anime_id)
+    # TODO: 外部キー活用してワンライナーで取得
+    anime_cvs = AnimeCV.objects.filter(anime_id=anime_id)
+    cv_list = CV.objects.filter(id__in=[anime_cv.cv_id for anime_cv in anime_cvs])
+    template = loader.get_template('anime.html')
+    context = {
+        'anime': anime,
+        'cv_list': cv_list,
     }
     return HttpResponse(template.render(context, request))
